e7561dc87ee92d08682ee2da5e1c57a7e0475c53
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-01-13 20:40:10 +0000

    remocao de arquivos antigos / inutilizados
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@18 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/application/layout/default.php
+++
@@ -1,5 +0,0 @@
-<b><?php echo time() . " (before)" ?></b>
-<hr>
-<?php $this->renderTemplate() ?>
-<hr>
-<i><?php echo date("Y-m-d H:i:s") . " (after)" ?></i>
--- trunk/application/view/Index/index.php
+++
@@ -1,3 +0,0 @@
-<h1><?php echo $this->login_email ?></h1>
-<h2><?php echo $this->created_at ?></h3>
-<h3><?php echo $this->void ?></h3>
