082c35c487b71c550c69d0bbb613d60d9287ca4b
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-02-01 12:55:56 +0000

    Improved the changelog calculator.
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@50 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/doc/sc.sh
+++ trunk/doc/sc.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 total=0
 changelog=$@
+days_worked=`grep HORAS $@ | wc -l | sed -e 's/[ ]*//g'`
+value_hour="14.5"
 
 for i in `grep HORAS $changelog | sed -e 's/HORAS TRABALHADAS: //g' | sed -e 's/h//g'`; do
     total=$(expr $i + $total); 
 done
 
-echo $total
+printf "Days worked:  %d\n" $days_worked
+printf "Hours worked: %d\n" $total
+printf "Avg hr p/day: %d\n" `expr $total / $days_worked`
+printf "Total:        %.2f\n" `echo $total \* $value_hour | bc`
