68b5f6f09e1076c5d0963d06986fb8ca5fb2109e
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-02-16 02:48:19 +0000

    First version of the backend, with inittial code for feed url identification.
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@64 59c7e291-545d-0410-9381-d2a3ada94e96

---
+++ trunk/backend/main.py
@@ -0,0 +1,42 @@
+# main.py --- main file of the backend system of blotomate
+
+# Copyright  (C)  2009  Marcelo Toledo <marcelo@marcelotoledo.org>
+
+# Version: 1.0
+# Keywords: 
+# Author: Marcelo Toledo <marcelo@marcelotoledo.org>
+# Maintainer: Marcelo Toledo <marcelo@marcelotoledo.org>
+# URL: http://
+
+# Commentary: 
+
+# Code:
+# find all rss feeds within one click of a URL
+
+VERSION = "1.0.0"
+
+import sys
+import feedfinder
+
+def usage():
+    print 'Blotomate Backend %s - Backend system for blotomate.com' % VERSION
+    print 'Copyright  (C)  2009 Blotomate Inc. <https://www.blotomate.com>\n'
+    
+    print 'Usage: %s [url]' % sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        usage()
+        sys.exit(-1)
+
+    feeds = feedfinder.feeds(sys.argv[1])
+
+    feeds_len = len(feeds)
+    
+    print "Feed for %s: " % sys.argv[1]
+    if feeds_len == 0:
+        print "No feed available."
+    else:
+        print "Preferencial: %s" % feeds[0]
+        if feeds_len > 1:
+            print "Others:       %s" % feeds[1:]
