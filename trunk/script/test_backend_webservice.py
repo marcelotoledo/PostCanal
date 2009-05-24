deabad950f8b04ddc38d97403777049cfa67c64d
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-05-24 18:32:16 +0000

    inclusao de script p teste do webservice de backend
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@259 59c7e291-545d-0410-9381-d2a3ada94e96

---
+++ trunk/script/test_backend_webservice.py
@@ -0,0 +1,20 @@
+import sys, os
+
+base_path = os.path.abspath("../")
+sys_path = base_path + "/backend"
+config_path = base_path + "/config/environment.xml"
+
+sys.path.append(sys_path)
+
+# ==============================================================================
+
+from blotomate import BlotomateConfig
+
+token = BlotomateConfig(config_path).get('webservice/token')
+
+import xmlrpclib
+
+#remote = "http://postcanal.com/backend.py"
+remote = "http://192.168.56.101/backend.py"
+server = xmlrpclib.ServerProxy(remote)
+print server.feed_discover({ 'token' : token, 'url' : 'http://test.wordpress.com' })
