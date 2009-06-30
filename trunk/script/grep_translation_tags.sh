1befea081514dea3144f4ff31ad33ec671e25010
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-06-30 16:39:49 +0000

    translations e small fixes
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@331 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/script/grep_translation_tags.sh
+++ trunk/script/grep_translation_tags.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-find ../application/view -type f | grep -v ".svn" | xargs grep "translation()->" | sed s/"^.*translation()->"//g | sed s/"\ .*"//g | sed s/"[^a-zA-Z0-9_]"//g | sort -u
+for file in $(find ../application/view -type f | \
+              grep -v ".svn" | \
+              grep -v "cache" | \
+              sort)
+do
+    echo "--------------------------------------------------------------------------------"
+    echo "$file"
+    echo "--------------------------------------------------------------------------------"
+
+    grep "translation()->" $file | \
+    sed s/"^.*translation()->"//g | \
+    sed s/"\ .*"//g | \
+    sed s/"[^a-zA-Z0-9_]"//g | \
+    sort -u
+done
