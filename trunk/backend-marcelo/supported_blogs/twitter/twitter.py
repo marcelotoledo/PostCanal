e0d702553926f780c8753e7d13344e49b3c24825
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-08-25 14:37:04 +0000

    Added support to bit.ly in twitter.
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@408 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/backend-marcelo/supported_blogs/twitter/twitter.py
+++ trunk/backend-marcelo/supported_blogs/twitter/twitter.py
@@ -6,8 +6,12 @@
 # 
 
 import sys
+import re
+import urllib2
 
 import ptt
+import bitly
+
 from blog import Blog
 
 
@@ -45,11 +49,29 @@ class Twitter(Blog):
     #___________________________________________________________________________
 
     def setContent(self, content):
-        if len(content) > Twitter.MAX_CHARS:
-            print 'Warning: only the first %d characters will be posted' % Twitter.MAX_CHARS
-            self._content = content[:Twitter.MAX_CHARS]
+        suffix = ''
+        # test if URL
+        if self.domain(content):
+            url = content
+            html = urllib2.urlopen(url).read()
+            title = re.search('<title>.*</title>', html, re.IGNORECASE).group()
+            if title:
+                # remove title tags
+                title = title.replace('<title>', '').replace('</title>', '')
+                # remove site info, if exists
+                if '|' in title: title = title[title.rfind('|')+1:].strip()
+                if '-' in title: title = title[:title.find('-')].strip()
+                # create shortened url with bitly
+                api = bitly.Api(login='pythonapi', apikey='R_20f7a20b92d38c21877ac4397fffadfb')
+                suffix = ' ' + api.shorten(url)
+                content = title
+        if len(content) + len(suffix) > Twitter.MAX_CHARS:
+            # shorten content
+            dots = '...'
+            content = content[:Twitter.MAX_CHARS - len(dots) - len(suffix)] + dots + suffix
         else:
-            self._content = content
+            content = content + suffix
+        self._content = content
 
     #___________________________________________________________________________
 
@@ -71,11 +93,31 @@ if __name__ == '__main__':
     else:
         api = Twitter(admin_url, username, password)
         if api.authenticate():
+            # test example cases
             api.setContent('my test post to Twitter ')
             api.postEntry()
 
             api.clear()
             api.setContent('unicode test - 汉语/漢語')
+            api.postEntry()
+
+            # test bitly
+            api.clear()
+            api.setContent('http://news.bbc.co.uk/2/hi/uk_news/scotland/highlands_and_islands/8217162.stm')
+            api.postEntry()
+
+            api.clear()
+            api.setContent('http://gizmodo.com/5343799/blackberry-bold-visual-voicemail-feature-now-live-os-drops-tuesday')
+            api.postEntry()
+
+            # test long text
+            api.clear()
+            api.setContent('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+            api.postEntry()
+
+            # test 140 chars
+            api.clear()
+            api.setContent('0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 ')
             api.postEntry()
         else:
             print 'Authentication failed'
