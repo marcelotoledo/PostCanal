c9930f963e37a6189bfb8f435ba14b76d52dd3a2
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-08-26 00:42:43 +0000

    Final version of twitter.
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@410 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/backend-marcelo/supported_blogs/twitter/twitter.py
+++ trunk/backend-marcelo/supported_blogs/twitter/twitter.py
@@ -48,36 +48,26 @@ class Twitter(Blog):
 
     #___________________________________________________________________________
 
-    def setContent(self, content):
-        suffix = ''
-        # test if URL
-        if self.domain(content):
-            url = content
-            html = urllib2.urlopen(url).read()
-            title = re.search('<title>.*</title>', html, re.IGNORECASE).group()
-            if title:
-                # remove title tags
-                title = title.replace('<title>', '').replace('</title>', '')
-                # remove site info, if exists
-                if '|' in title: title = title[title.rfind('|')+1:].strip()
-                if '-' in title: title = title[:title.find('-')].strip()
-                # create shortened url with bitly
-                api = bitly.Api(login='pythonapi', apikey='R_20f7a20b92d38c21877ac4397fffadfb')
-                suffix = ' ' + api.shorten(url)
-                content = title
-        if len(content) + len(suffix) > Twitter.MAX_CHARS:
-            # shorten content
-            dots = '...'
-            content = content[:Twitter.MAX_CHARS - len(dots) - len(suffix)] + dots + suffix
-        else:
-            content = content + suffix
-        self._content = content
+    def setTitle(self, title):
+        self._title = title
+    #___________________________________________________________________________
 
+    def setContent(self, content):
+        # create shortened url with bitly
+        api = bitly.Api(login='pythonapi', apikey='R_20f7a20b92d38c21877ac4397fffadfb')
+        self._content = ' ' + api.shorten(content)
     #___________________________________________________________________________
 
     def postEntry(self):
         if self._authenticated:
-            self.api.statuses.update(status=self._content)
+            # test if URL
+            if len(self._title) + len(self._content) > Twitter.MAX_CHARS:
+                # shorten content
+                dots = '...'
+                status = self._title[:Twitter.MAX_CHARS - len(dots) - len(self._content)] + dots + self._content
+            else:
+                status = self._title + self._content
+            self.api.statuses.update(status=status)
         else:
             print 'Need to authenticate'
 
@@ -94,30 +84,32 @@ if __name__ == '__main__':
         api = Twitter(admin_url, username, password)
         if api.authenticate():
             # test example cases
-            api.setContent('my test post to Twitter ')
+            api.setTitle('my test post to Twitter ')
             api.postEntry()
 
             api.clear()
-            api.setContent('unicode test - 汉语/漢語')
+            api.setTitle('unicode test - 汉语/漢語')
             api.postEntry()
 
             # test bitly
             api.clear()
+            api.setTitle('Loch canoeists die after rescue')
             api.setContent('http://news.bbc.co.uk/2/hi/uk_news/scotland/highlands_and_islands/8217162.stm')
             api.postEntry()
 
             api.clear()
+            api.setTitle('BlackBerry Bold Visual Voicemail Feature Now Live, OS Drops Tuesday [BlackBerry]')
             api.setContent('http://gizmodo.com/5343799/blackberry-bold-visual-voicemail-feature-now-live-os-drops-tuesday')
             api.postEntry()
 
             # test long text
             api.clear()
-            api.setContent('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+            api.setTitle('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
             api.postEntry()
 
             # test 140 chars
             api.clear()
-            api.setContent('0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 ')
+            api.setTitle('0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 ')
             api.postEntry()
         else:
             print 'Authentication failed'
