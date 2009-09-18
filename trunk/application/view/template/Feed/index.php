200f8a2e2bd45c8ec99157b88e207b547a5dba85
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-09-18 22:45:32 +0000

    layout smallfixes
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@565 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/application/view/template/Feed/index.php
+++ trunk/application/view/template/Feed/index.php
@@ -9,6 +9,7 @@
     <div class="form-row">
     <p>URL</p><p><input type="text" class="intxt intxt-full" id="feedurl"></p>
     </div>
+    <div id="supportlist">Today we support RSS and Atom feeds.</div>
     <div class="form-bot">
     <button id="addsubmit">CONTINUE</button>
     <button id="addcancel">CANCEL</button>
