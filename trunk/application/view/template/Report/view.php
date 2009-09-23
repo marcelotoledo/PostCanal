b9f20a5e35953aad274275482451feb2f665f055
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-09-23 18:16:32 +0000

    inclusao de modulo para report
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@614 59c7e291-545d-0410-9381-d2a3ada94e96

---
+++ trunk/application/view/template/Report/view.php
@@ -0,0 +1,24 @@
+<h1>Report</h1>
+
+<h2>View</h2>
+
+<p><a href="./report">go back</a></p>
+
+<p>
+<table id="report-view" style="border:2px solid gray">
+<?php foreach($this->result AS $l => $i) : ?>
+    <?php if($l==0) : ?>
+    <tr>
+    <?php foreach(array_keys($i) as $c) : ?>
+        <th><?php echo $c; ?></th>
+    <?php endforeach ?>
+    </tr>
+    <?php endif ?>
+    <tr>
+    <?php foreach($i as $c => $j) : ?>
+        <td><?php echo strlen($j)>0 ? $j : "&nbsp;" ?></td>
+    <?php endforeach ?>
+    </tr>
+<?php endforeach ?>
+</table>
+</p>
