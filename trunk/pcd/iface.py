e0e07d975840409e3284a6b7b3efe99f942e79b9
Marcelo Toledo <marcelo@marcelotoledo.com>
2009-12-18 01:01:18 +0000

    Timeout implemented in the interface.
    git-svn-id: svn+ssh://localhost/var/svn/postcanal@695 59c7e291-545d-0410-9381-d2a3ada94e96

--- trunk/pcd/iface.py
+++ trunk/pcd/iface.py
@@ -17,14 +17,42 @@ import xmlrpclib
 
 l = log.Log()
 
+# def openConnection(url):
+#     if url == None:
+#         return None
+    
+#     try:
+#         client = xmlrpclib.ServerProxy(url)
+#     except:
+#         l.log("webservice call failed; (%s)" % (sys.exc_info()[0].__name__), funcName())
+#         return None
+    
+#     return client
+
 def openConnection(url):
     if url == None:
         return None
-    
+
     try:
-        client = xmlrpclib.ServerProxy(url)
+        client = xmlrpclib.ServerProxy(url, transport=TimeoutTransport())
     except:
         l.log("webservice call failed; (%s)" % (sys.exc_info()[0].__name__), funcName())
         return None
     
     return client
+
+class TimeoutTransport(xmlrpclib.Transport):
+    """Add a timeout attibute to HTTPConnection through HTTP."""
+    
+    def __init__(self, use_datetime=0, timeout=60):
+        xmlrpclib.Transport.__init__(self, use_datetime)
+        self.timeout = timeout
+
+    def make_connection(self, host):
+        """
+        Extends Transport.make_connection to set the timeout on the
+        connection instance.
+        """
+        conn = xmlrpclib.Transport.make_connection(self, host)
+        conn._conn.timeout = self.timeout
+        return conn
